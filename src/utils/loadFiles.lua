f37cbfffbf001515f54df25bb76facdb99c54a84
Luke Sahula <lukas.sahula@icloud.com>
2020-09-14 11:46:40 +0200

    Fix load file function

--- src/utils/loadFiles.lua
+++ src/utils/loadFiles.lua
@@ -2,9 +2,10 @@ function loadFiles(folder, file_list)
   local items = love.filesystem.getDirectoryItems(folder)
   for _, item in ipairs(items) do
     local file = folder .. '/' .. item
-    if love.filesystem.isFile(file) then
+    local info = love.filesystem.getInfo(file)
+    if info.type == 'file' then
       table.insert(file_list, file)
-    elseif love.filesystem.isDirectory(file) then
+    elseif info.type == 'directory' then
       loadFiles(file, file_list)
     end
   end
